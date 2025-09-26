0f4908f4bcf1d6acc1835a2a4e90d37e067e87ed
Hans_Lando <hanslando.developer@outlook.de>
2025-09-26 19:15:00 +0200

    modifications to KnockKnockServer

--- src/main/java/Sockets/KnockKnock/KnockKnockServer.java
+++ src/main/java/Sockets/KnockKnock/KnockKnockServer.java
@@ -9,35 +9,33 @@ public class KnockKnockServer {
     public static void main(String[] args) {
 
 
-        try(ServerSocket serverSocket=new ServerSocket(4444)){
-            System.out.println("Waiting for the client...");
+        System.out.println("Waiting for the client...");
 
-            try(Socket communicationSocket=serverSocket.accept();
-                BufferedReader serverInput=new BufferedReader(new InputStreamReader(communicationSocket.getInputStream()));
-                PrintWriter serverOutput=new PrintWriter(new OutputStreamWriter(communicationSocket.getOutputStream()),true)
-            ){
-                System.out.println("Client is connected.");
+        try (ServerSocket serverSocket = new ServerSocket(4444);
+             Socket communicationSocket = serverSocket.accept();
+             BufferedReader serverInput = new BufferedReader(new InputStreamReader(communicationSocket.getInputStream()));
+             PrintWriter serverOutput = new PrintWriter(new OutputStreamWriter(communicationSocket.getOutputStream()), true)
+        ) {
+            System.out.println("Client is connected.");
 
-                String inputLine, outputLine;
-                KnockKnockProtocol kkp = new KnockKnockProtocol();//keeps track of the conversation
-                outputLine=kkp.processInput(null);//Initiating conversation by finding out the reply for a null input
-                serverOutput.println(outputLine);//Sending the initial greeting
+            String inputLine, outputLine;
+            KnockKnockProtocol kkp = new KnockKnockProtocol();//keeps track of the conversation
+            outputLine = kkp.processInput(null);//Initiating conversation by finding out the reply for a null input
+            serverOutput.println(outputLine);//Sending the initial greeting
 
-                while(
-                        (inputLine=serverInput.readLine())
-                                !=null){//if the inputLine is not null after having have read the server input
+            while (
+                    (inputLine = serverInput.readLine())
+                            != null) {//if the inputLine is not null after having have read the server input
 
-                    if (inputLine.equalsIgnoreCase("Bye!")){
-                        break;
-                    }
-                    outputLine=kkp.processInput(inputLine);//finding out what output to send
-                    serverOutput.println(outputLine);//sending respond to the client
-
+                if (inputLine.equalsIgnoreCase("Bye!")) {
+                    break;
                 }
+                outputLine = kkp.processInput(inputLine);//finding out what output to send
+                serverOutput.println(outputLine);//sending respond to the client
 
             }
-
-        } catch (IOException e) {
+        } catch (
+                IOException e) {
             throw new RuntimeException(e);
         }
     }
