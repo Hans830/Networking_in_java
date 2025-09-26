3374e7fdae25edecb9d2284a5e4939aa75cab0e0
Hans_Lando <hanslando.developer@outlook.de>
2025-09-26 22:52:37 +0200

    -Changes to client

--- src/main/java/Datagrams/Multicast/MulticastClient.java
+++ src/main/java/Datagrams/Multicast/MulticastClient.java
@@ -14,13 +14,16 @@ public class MulticastClient {
         MulticastSocket clientSocket=new MulticastSocket(4446);
         InetAddress group=InetAddress.getByName("239.0.0.1");
         clientSocket.joinGroup(group);
-
+        System.out.println("Joint ClientGroup");
         DatagramPacket packet;
         byte[] buf = new byte[256];
         for(int i=0;i<5;i++){
 
+
             packet= new DatagramPacket(buf,buf.length);
+            System.out.println("Recieving packet.");
             clientSocket.receive(packet);
+            System.out.println("Packet recieved.");
 
             System.out.println("Quote: of the Moment: "+new String(packet.getData()));
         }
